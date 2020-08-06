b042d2060b78c585b958c78686401607e31b33b1
fabricat <1492255+fabricat@users.noreply.github.com>
2020-08-06 13:04:13 +0200

    show exception
    otherwise scripts fails too silently
    
    Signed-off-by: Fabrizio Catalucci <fabrizio.catalucci@konicaminolta.it>

--- storcli.py
+++ storcli.py
@@ -25,6 +25,7 @@ import json
 import os
 import shlex
 import subprocess
+import sys, traceback
 
 DESCRIPTION = """Parses StorCLI's JSON output and exposes MegaRAID health as
     Prometheus metrics."""
@@ -55,7 +56,7 @@ def main(args):
             elif response['Version']['Driver Name'] == 'mpt3sas':
                 handle_sas_controller(response)
     except KeyError:
-        pass
+        traceback.print_exc()
 
     print_all_metrics(metric_list)
 
